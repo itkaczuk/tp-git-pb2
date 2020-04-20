e0866441462ad6ad0742193dbb53c8552dafbb9a
ivann <ivann@192.168.0.14>
2020-04-20 18:05:08 -0300

    Cambio de if-else a switch

--- TP1-GIT/src/ar/edu/unlam/pb2/tp1/Calculadora.java
+++ TP1-GIT/src/ar/edu/unlam/pb2/tp1/Calculadora.java
@@ -2,30 +2,34 @@ package ar.edu.unlam.pb2.tp1;
 
 import java.util.Scanner;
 
-public class Ahorcado {
+public class Calculadora {
 	
 	public static void main(String[] args) {
-			Integer a;
-			Integer b;
-			Integer opcion;
-			
-			do{
-				System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir"); 
-				opcion = teclado.next();
-			}while(opcion < 1 && opcion > 4)
-				System.out.println("Ingrese el primer numero");
-			a = teclado.next();
-			System.out.println("Ingrese el segundo numero");
-			a = teclado.next();
-			if(opcion.equals(1)){
-				resultado = a + b;
-			}else if(opcion.equals(2)){
-				resultado = a - b;
-			}else if(opcion.equals(3)){
-				resultado = a * b;
-			}else{
-				resultado = a / b;
-			}
-			System.out.println("El resultado es " + resultado.toString());
-			}
-}
+		Integer a = 0;
+		Integer b = 0;
+		Integer opcion = 0;
+		Integer resultado = 0;
+		Scanner teclado = new Scanner(System.in);
+		
+		do{
+			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir"); 
+			opcion = teclado.nextInt();
+		}while(opcion < 1 && opcion > 4);
+			System.out.println("Ingrese el primer numero");
+		a = teclado.nextInt();
+		System.out.println("Ingrese el segundo numero");
+		a = teclado.nextInt();
+		if(opcion.equals(1)){
+			resultado = a + b;
+		}else if(opcion.equals(2)){
+			resultado = a - b;
+		}else if(opcion.equals(3)){
+			resultado = a * b;
+		}else{
+			resultado = a / b;
+		}
+		System.out.println("El resultado es " + resultado.toString());
+		}
+	}
+
+		
